ab9ed6319a4237100c24467a9bd4e981f9924197
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-18 11:04:10 +0000

    Adding the s

--- GenerateNonsense/index.js
+++ GenerateNonsense/index.js
@@ -1,4 +1,4 @@
-module.export = async function (context, req) {
+module.exports = async function (context, req) {
     context.log('Generating Nonsense...');
     context.res = {
         body: "Bibble wibble wobble"
